cb55d78989dd9ff1b04059ec8401b4745fbb1b33
OlivierBelaud <olivierbelaudpro@gmail.com>
2020-08-17 17:14:11 +0100

    feat: ensure RemoteAuthentication call httpPostClient with correct body

---
+++ src/domain/test/mock-authentication.ts
@@ -0,0 +1,8 @@
+import { AuthenticationParams } from '../usecases/authentication'
+import faker from 'faker'
+
+// Fonction qui retourne un object
+export const mockAuthentication = (): AuthenticationParams => ({
+  email: faker.internet.email(),
+  password: faker.internet.password()
+})
